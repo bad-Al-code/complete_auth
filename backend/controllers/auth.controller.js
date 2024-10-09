8730edc8102ff83e2d57ffe34383b587b05c53f8
badAl <badalsingh120403@gmail.com>
2024-10-09 17:29:28 +0530

    fix: use 500 statusCode is verifyEmail is failed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,7 +82,8 @@ export const verifyEmail = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(400).json({ success: false, message: error.message });
+    console.log("Error in verifyEmail", error);
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
