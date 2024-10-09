d6ee75e7bd2f9682c1e60164c50692e71f940589
badAl <badalsingh120403@gmail.com>
2024-10-09 14:10:38 +0530

    declare basic auth controllers

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
