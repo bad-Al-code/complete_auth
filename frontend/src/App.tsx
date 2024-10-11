e182d030e9bb2d6e4d3338013b9781e2bb7da306
badAl <badalsingh120403@gmail.com>
2024-10-11 16:30:23 +0530

    Add TextButtonWithIcons compoentn with a dynamic icons and label

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-import Input from "./components/Input";
+import TextButtonWithIcon from "./TextButtonWithIcon";
 
 const App: React.FC = () => {
+  const handleClick = () => {};
   return (
-    <div className="flex flex-wrap  items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
-      <Input label="Input" />
+    <div className="flex flex-col flex-wrap  items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
+      <TextButtonWithIcon
+        onClick={handleClick}
+        label="Go BackWhere you came from"
+      />
     </div>
   );
 };
