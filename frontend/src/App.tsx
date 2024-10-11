7b05ec26728cf93daefe7e602871f3e0380ee920
badAl <badalsingh120403@gmail.com>
2024-10-11 16:34:29 +0530

    Checkbox with text on right side component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import Checkbox from "./components/Checkbox";
 import TextButtonWithIcon from "./TextButtonWithIcon";
 
 const App: React.FC = () => {
-  const handleClick = () => {};
+  const handleCheckboxChange = () => {};
   return (
     <div className="flex flex-col flex-wrap  items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
-      <TextButtonWithIcon
-        onClick={handleClick}
-        label="Go BackWhere you came from"
+      <Checkbox
+        id="agree"
+        onChange={handleCheckboxChange}
+        label="Agree to "
+        linkText="Terms and Conditions"
+        linkHref="#"
       />
     </div>
   );
