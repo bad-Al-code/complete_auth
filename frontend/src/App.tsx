838b6bb661174cee39afb9ab275ebb6be71377e0
badAl <badalsingh120403@gmail.com>
2024-10-11 17:18:36 +0530

    completed all pages for auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+import LoginPage from "./pages/auth/LoginPage";
 import OtpVerificationPage from "./pages/auth/OTPVerificationPage";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import SignupPage from "./pages/auth/SignupPage";
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col flex-wrap  items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
+      <SignupPage />
       <OtpVerificationPage />
+      <LoginPage />
+      <ForgotPasswordPage />
+      <ResetPasswordPage />
     </div>
   );
 };
