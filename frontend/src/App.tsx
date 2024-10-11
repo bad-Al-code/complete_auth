dc436ca4ff0491dd79532b60b07682906d4a189c
badAl <badalsingh120403@gmail.com>
2024-10-11 15:58:59 +0530

    created input compoent for reusabilty

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import Button from "./components/Button";
+import Input from "./components/Input";
 
 const App: React.FC = () => {
   return (
-    <>
-      <Button label="Button" fullWidth />
-    </>
+    <div className="flex flex-wrap  items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
+      <Input label="Input" />
+    </div>
   );
 };
 
