9c239a1cb14f4b67aeb0e0ecb15c6ca33dba39a7
badAl <badalsingh120403@gmail.com>
2024-10-27 02:10:32 +0530

    ripple effect

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,37 +2,43 @@ import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: "#fff7ed",
-          100: "#ffedd5",
-          200: "#fed7aa",
-          300: "#fdba74",
-          400: "#fb923c",
-          500: "#f97316",
-          600: "#ea580c",
-          700: "#c2410c",
-          800: "#9a3412",
-          900: "#7c2d12",
-          950: "#431407",
+    darkMode: "class",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#ee75d2",
+                secondary: "#75d8ee",
+                tertiary: "#deee75",
+                quaternary: "#9375ee",
+                surface: "#000000",
+            },
+            backgroundImage: {
+                bgGradient:
+                    "linear-gradient(to bottom, var(--color-quaternary), var(--color-surface))",
+            },
+            animation: {
+                ripple: "ripple 1s ease-in-out infinite",
+            },
+            keyframes: {
+                ripple: {
+                    "0%": { transform: "scale(1)", opacity: "1" },
+                    "50%": { transform: "scale(1.5)", opacity: "0.4" },
+                    "100%": { transform: "scale(1)", opacity: "1" },
+                },
+            },
         },
-      },
     },
-  },
-  plugins: [addVariablesForColors],
+    plugins: [addVariablesForColors],
 };
 
 function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
 
-  addBase({
-    ":root": newVars,
-  });
+    addBase({
+        ":root": newVars,
+    });
 }
